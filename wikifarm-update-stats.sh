f39965a2d42fe942e46d834db5f15108e9ec05b2
Tom Clegg <tom@clinicalfuture.com>
2010-07-11 17:33:28 -0400

    awstats

---
+++ wikifarm-update-stats.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+INSTALLDIR=${INSTALLDIR:-/home/wikifarm}
+
+set -e
+set -x
+
+sqlite3 -separator ' ' /home/wikifarm/db/wikis.db \
+ 'select id, wikiname from wikis' | while read wikiid wikiname
+do
+  wikiid=`printf %02d $wikiid`
+  echo $wikiid
+  cat >${INSTALLDIR}/wikis/$wikiid/private/index.html <<EOF
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="/style.css">
+</head>
+<body>
+<br>
+<br>
+Backups
+<ul>
+<li>pub is mirrored nightly at 3:30 am, and backed up to tape at 4:30 am.</li>
+<li>contact Shawn if you would like to download a local copy of your wiki.</li>
+<!--<li><a href="$wikiname-backup.tar">$wikiname-backup.tar</a></li>-->
+</ul>
+Access Statistics
+<ul>
+<li><a href="stats/awstats.$wikiid.html">report</a>
+<li><a href="access_log.txt">raw access_log</a>
+</ul>
+</body>
+</html>
+EOF
+  builder=$INSTALLDIR/etc/awstats_buildstaticpages.pl
+  wikidir=$INSTALLDIR/wikis/$wikiid/private/stats
+  cd $wikidir
+  perl -pe "s:/00/:/$wikiid/:g;" <$INSTALLDIR/etc/awstats.00.conf >awstats.$wikiid.conf
+  perl $builder -awstatsprog=/usr/lib/cgi-bin/awstats.pl -configdir=$wikidir -config=$wikiid -update -dir=$wikidir
+done
