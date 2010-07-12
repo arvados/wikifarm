bc0f995e0d6affaae6deb31fbb32f500eba69d44
Tom Clegg <tom@clinicalfuture.com>
2010-07-11 21:13:48 -0400

    add "all stats" report to wiki 01

--- wikifarm-update-stats.sh
+++ wikifarm-update-stats.sh
@@ -5,11 +5,29 @@ INSTALLDIR=${INSTALLDIR:-/home/wikifarm}
 set -e
 set -x
 
+builder=$INSTALLDIR/etc/awstats_buildstaticpages.pl
+
+# "all" stats go in /01/private/stats
+
+wikiid_allstats=01
+wikidir=$INSTALLDIR/wikis/$wikiid_allstats/private/stats
+cd $wikidir
+perl -pe "s:/00/:/$wikiid_allstats/:g;" <$INSTALLDIR/etc/awstats.all.conf >awstats.all.conf
+perl $builder -awstatsprog=/usr/lib/cgi-bin/awstats.pl -configdir=$wikidir -config="all" -update -dir=$wikidir
+
+
+# Individual wiki stats go in /##/private/stats
+
 sqlite3 -separator ' ' /home/wikifarm/db/wikis.db \
  'select id, wikiname from wikis' | while read wikiid wikiname
 do
   wikiid=`printf %02d $wikiid`
-  echo $wikiid
+
+  adminlink=""
+  if [ "$wikiid" = "$wikiid_allstats" ]
+  then
+    adminlink="<li><a href=\"stats/awstats.all.html\">report for all wikis</a></li>"
+  fi
   cat >${INSTALLDIR}/wikis/$wikiid/private/index.html <<EOF
 <html>
 <head>
@@ -26,13 +44,13 @@ Backups
 </ul>
 Access Statistics
 <ul>
-<li><a href="stats/awstats.$wikiid.html">report</a>
-<li><a href="access_log.txt">raw access_log</a>
+<li><a href="stats/awstats.$wikiid.html">report</a></li>
+<li><a href="access_log.txt">raw access_log</a></li>
+$adminlink
 </ul>
 </body>
 </html>
 EOF
-  builder=$INSTALLDIR/etc/awstats_buildstaticpages.pl
   wikidir=$INSTALLDIR/wikis/$wikiid/private/stats
   cd $wikidir
   perl -pe "s:/00/:/$wikiid/:g;" <$INSTALLDIR/etc/awstats.00.conf >awstats.$wikiid.conf
