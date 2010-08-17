542bd7746b6f115b3029e600607634697b009ded
Tom Clegg <tom@clinicalfuture.com>
2010-08-17 18:59:38 -0400

    support multiple mwusernames per oid user, confer sysop rights from autologin

--- WikifarmAuthPlugin.php
+++ WikifarmAuthPlugin.php
@@ -5,20 +5,37 @@ class WikifarmAuthPlugin extends AuthPlugin {
 
 	var $db;
 	var $userid;
+	var $userid_is_owner;
+	var $userid_is_admin;
+	var $userrow;
+	var $mwusername;
 
 	function __construct () {
 		$this->db = new SQLite3 (getenv("WIKIFARM_DB_FILE"));
 		$this->userid = $_SERVER["REMOTE_USER"];
-		$this->mwusername = $this->db->querySingle
-			("select mwusername from autologin where wikiid='"
+		$autologin = $this->db->querySingle
+			("select mwusername, sysop from autologin where wikiid='"
 			 . getenv("WIKIID")
 			 . "' and userid='"
 			 . SQLite3::escapeString ($this->userid)
-			 . "' order by lastlogintime desc limit 1");
+			 . "' order by lastlogintime desc limit 1", true);
+		if ($autologin) {
+			$this->mwusername = $autologin["mwusername"];
+			$this->userid_is_owner = $autologin["sysop"];
+		}
+
 		$this->userrow = $this->db->querySingle
 			("select * from users where userid='"
 			 . SQLite3::escapeString ($this->userid) . "'",
-			 TRUE);
+			 true);
+		if (!$this->userid_is_owner)
+			$this->userid_is_owner = $this->db->querySingle
+				("select 1 from wikis where id='"
+				 . getenv("WIKIID") . "' and userid='"
+				 . SQLite3::escapeString ($this->userid) . "'");
+		$this->userid_is_admin = $this->db->querySingle
+			("select 1 from usergroups where userid='"
+			 . SQLite3::escapeString ($this->userid) . "' and groupname='ADMIN'");
 	}
 
 	public function userExists( $username ) {
@@ -28,7 +45,9 @@ class WikifarmAuthPlugin extends AuthPlugin {
 	}
 
 	public function authenticate( $username, $password ) {
-		return $this->mwusername !== false && $this->mwusername == $username;
+		return isset ($this->mwusername) &&
+			$this->mwusername !== false &&
+			$this->mwusername == $username;
 	}
 
 	public function modifyUITemplate( &$template ) {
@@ -44,7 +63,7 @@ class WikifarmAuthPlugin extends AuthPlugin {
 	}
 
 	public function updateUser( &$user ) {
-		error_log ("updateUser");
+		if (!$this->userrow) return true;
 		if ($this->userrow["email"])
 			$user->mEmail = $this->userrow["email"];
 		if ($this->userrow["realname"])
@@ -101,21 +120,23 @@ class WikifarmAuthPlugin extends AuthPlugin {
 
 	function autoAuthenticate ($user) {
 		if (!isset($this->mwusername))
-			return false;
+			return true;
+
+		$freshlogin = false;
 
 		global	$wgCommandLineMode;
-		if( !$wgCommandLineMode && !isset( $_COOKIE[session_name()] )  ) {
+		if( !$wgCommandLineMode && (!$_SESSION ||
+					    ($_SESSION["wsUserName"] != "" &&
+					     $_SESSION["wsUserName"] != $this->mwusername))) {
+			$_SESSION = array();
+			global $wgCookiePrefix;
+			foreach ($_COOKIE as $k => $v) {
+				if (strncmp ($k, $wgCookiePrefix, strlen($wgCookiePrefix)) == 0)
+					unset ($_COOKIE[$k]);
+			}
 			User::SetupSession();
+			$freshlogin = true;
 		}
-		if ( method_exists($user, 'load' ) ) {
-			$user->load();
-		} else {
-			User::loadFromSession();
-		}
-		if($user->mId != 0) {
-			return 0;
-		}
-
 		$user = User::newFromName($this->mwusername);
 		if($user->getID() == 0) {
 			$user->addToDatabase();
@@ -125,19 +146,28 @@ class WikifarmAuthPlugin extends AuthPlugin {
 		} else {
 			$user->loadFromDatabase();
 		}
+
 		$user->setCookies();
 		$user->saveSettings();
-		$this->updateLastLoginTime();
+
+		if ($freshlogin)
+			$this->updateLastLoginTime();
+
 		return true;
 	}
 
-	public function addAutoLogin ( $user ) {
+	// UserLoginComplete: when a user logs in using MediaWiki's
+	// built-in username/password system, add a corresponding row
+	// to the autologin table.
+
+	public function UserLoginComplete ( $user ) {
 		if (!$this->mwusername) {
 			$this->db->query ("insert into autologin "
-					  . "(wikiid, userid, mwusername) values ('"
+					  . "(wikiid, userid, mwusername, sysop) values ('"
 					  . getenv("WIKIID") . "', '"
 					  . SQLite3::escapeString ($this->userid) . "', '"
-					  . SQLite3::escapeString ($user->getName()) . "')");
+					  . SQLite3::escapeString ($user->getName()) . "', "
+					  . (array_key_exists ("sysop", $user->getGroups()) ? 1 : 0) . ")");
 			$this->mwusername = $user->getName();
 		}
 		if (!$this->userrow)
@@ -145,9 +175,22 @@ class WikifarmAuthPlugin extends AuthPlugin {
 					  . SQLite3::escapeString ($this->userid) . "', '"
 					  . SQLite3::escapeString ($user->mEmail) . "', '"
 					  . SQLite3::escapeString ($user->mRealName) . "')");
-		$this->updateLastLoginTime();
 		return true;
 	}
+
+	// UserGetRights: add sysop rights if this OpenID owns the
+	// wiki or is a site admin
+
+	public function UserGetRights ( $user, &$rights ) {
+		if ($this->userid_is_owner || $this->userid_is_admin)
+			$rights = array_merge ( $rights, User::getGroupPermissions (array ("sysop")) );
+		return true;
+	}
+
+	// updateLastLoginTime: remember the last login time so we can
+	// log in to the same MW user account next time (in case the
+	// OpenID has access to multiple MW user accounts in this
+	// wiki)
 
 	function updateLastLoginTime() {
 		$this->db->query("update autologin set lastlogintime=strftime('%s','now') where wikiid='"
@@ -160,4 +203,7 @@ class WikifarmAuthPlugin extends AuthPlugin {
 session_start();
 $wgAuth = new WikifarmAuthPlugin();
 $wgHooks['UserLoadFromSession'][] = array ($wgAuth, 'autoAuthenticate');
-$wgHooks['UserLoginComplete'][] = array ($wgAuth, 'addAutoLogin');
+$wgHooks['UserLoginComplete'][] = array ($wgAuth, 'UserLoginComplete');
+$wgHooks['UserGetRights'][] = array ($wgAuth, 'UserGetRights');
+global $wgDisableCookieCheck;
+$wgDisableCookieCheck = true;
