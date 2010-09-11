b86423db4da8ee5588b8f84418a25eec7a92608e
Tom Clegg <tom@clinicalfuture.com>
2010-09-11 07:23:24 -0400

    fix wrong variable name

--- WikifarmDriver.php
+++ WikifarmDriver.php
@@ -421,9 +421,9 @@ class WikifarmDriver {
 
 		// non-admin can only approve a request concerning a wiki she actually owns
 		if ($this->querySingle("select 1 from wikis where id=".$req[0]["wikiid"]." and userid='".$this->q_openid."'"))
-			return $reqs[0];
+			return $req[0];
 
-		error_log ("canApproveRequest: user ".$this->openid." cannot do ".print_r($reqs[0],true));
+		error_log ("canApproveRequest: user ".$this->openid." cannot do ".print_r($req[0],true));
 		return false;
 	}
 	
