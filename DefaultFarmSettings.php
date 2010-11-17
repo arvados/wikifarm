32b717511980ce207ff8f92fa74aa68f0a1b6db6
Tom Clegg <tom@clinicalfuture.com>
2010-11-16 16:49:52 -0800

    fix preg delimiters in DefaultFarmSettings

--- DefaultFarmSettings.php
+++ DefaultFarmSettings.php
@@ -11,7 +11,7 @@ $wgUseTeX = file_exists ("/usr/bin/texvc");
 
 $wgFileExtensions = array_merge ($wgFileExtensions, array('tar', 'gz', 'png', 'gif', 'jpg', 'jpeg', 'ogg', 'zip', 'ai', 'xls', 'doc', 'docx', 'py', 'pl', 'sh', 'tiff', 'bmp', 'txt', 'csv', 'pdf', 'ppt', 'key', 'mov', 'mpg', 'mpeg', 'avi', 'wmv', 'xvid', 'svg', 'svgz', 'json', 'ma', 'mb', 'xml', 'm'));
 $wgVerifyMimeType = false;
-$wgFileBlacklist = preg_grep ('^(php|php3|php4|php5|phps|phtml|shtml|jhtml|pl|py|cgi)$', $wgFileBlacklist, PREG_GREP_INVERT);
+$wgFileBlacklist = preg_grep ('/^(php|php3|php4|php5|phps|phtml|shtml|jhtml|pl|py|cgi)$/', $wgFileBlacklist, PREG_GREP_INVERT);
 
 require_once( "/etc/mediawiki-extensions/extensions.php" );
 
