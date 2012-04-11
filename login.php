a85b9110b2b80ca6bfcfe4067b727ff44fd81945
Tom Clegg <tom@clinicalfuture.com>
2012-04-11 15:19:32 -0400

    update login page text, add "servertitle" config knob. refs #882

--- login.php
+++ login.php
@@ -1,5 +1,6 @@
 <html><head>
-<title>Protected Location</title>
+<? @include(getenv('WIKIFARM_ETC').'/config.php'); ?>
+<title><?=$_SERVER['HTTP_HOST']?></title>
 <style type="text/css"><!--
 #msg { border: 1px solid #ff0000; background: #ffaaaa; font-weight: bold; padding: 10px; }
 a { text-decoration: none; }
@@ -13,35 +14,35 @@ a:hover { text-decoration: underline; }
 
 <body>
 <div style="margin: 30px; width: 50%">
-<h1>Protected Location</h1>
-<p>This site is protected and requires that you identify yourself with an <a href="http://openid.net">OpenID</a> url.</p>
+<h1><?= @$wikifarmConfig["servertitle"] ? $wikifarmConfig["servertitle"] : $_SERVER['HTTP_HOST']?></h1>
+<p>This site is protected and requires that you identify yourself with an <a href="http://openid.net">OpenID</a> login.</p>
 
 <? if(isset($_GET["modauthopenid_error"])) { ?>
 <div style="background: #fdd; border: 1px dashed #b00">Login failed (error code: <?=$_GET["modauthopenid_error"]?>)</div>
 <? } ?>
 
-<form class="marg" action="/" method="get" class="openidloginform">
-<b>Identity URL:</b> <input type="text" name="openid_identifier" value="" size="30" class="loginbox" />
-<input type="submit" value="Log In" />
-<input type="hidden" name="modauthopenid_referrer" value="<?=htmlspecialchars($_GET["modauthopenid_referrer"])?>" />
-</form>
-
 <form style="display:inline" action="/" method="get" class="openidloginform">
-<b class="marg">Shortcut:</b>
+<b class="marg">Log in via:</b>
 <input type="hidden" name="openid_identifier" value="https://www.google.com/accounts/o8/id" />
-<input class="marg" type="submit" value="Google Login" />
+<input class="marg" type="submit" value="Google" />
 <input type="hidden" name="modauthopenid_referrer" value="<?=htmlspecialchars($_GET["modauthopenid_referrer"])?>" />
 </form>
 
 <form style="display:inline" action="/" method="get" class="openidloginform">
 <input type="hidden" name="openid_identifier" value="http://open.login.yahooapis.com/openid20/www.yahoo.com/xrds" />
-<input class="marg" type="submit" value="Yahoo Login" />
+<input class="marg" type="submit" value="Yahoo" />
 <input type="hidden" name="modauthopenid_referrer" value="<?=htmlspecialchars($_GET["modauthopenid_referrer"])?>" />
 </form>
 
-<p>If you are surprised to see this because your browser has always just asked for username and password here, fear not.  After you log in with your OpenID for the first time, you can claim your lab notebook (if you have one), and get permission to view other lab members&rsquo; lab notebooks, by entering your old username and password.  After doing that once, you&rsquo;ll just need OpenID.</p>
+<p><b>or</b></p>
 
-<p>To find out how OpenID works, see <a href="http://openid.net/what/">http://openid.net/what/</a>.  You can sign up for an identity on one of <a href="http://openid.net/get/">these sites</a>.</p>
+<form class="marg" action="/" method="get" class="openidloginform">
+<b>URL:</b> <input type="text" name="openid_identifier" value="" size="30" class="loginbox" />
+<input type="submit" value="Log In" />
+<input type="hidden" name="modauthopenid_referrer" value="<?=htmlspecialchars($_GET["modauthopenid_referrer"])?>" />
+</form>
+
+<p>If you do not have an account, you may request one after logging in.</p>
 
 </div>
 <body>
