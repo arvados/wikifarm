arvados/wikifarm

commit 2ffd4c11e1d8fc484f2fbe6a31eb9f767ea424d4

Allow admins and wiki owners to control the per-wiki "active" flag (used by the "show/hide inactive wikis" filter on the All Wikis dashboard tab).

WikifarmPageMachine.php
@@ -232,7 +232,6 @@ BLOCK;
 		$wikiArray = $this->getAllWikis();
 		foreach ($wikiArray as $row) {
 			extract ($row);
-			$active = preg_match('{://}', $userid);
 			$requested_writable = $requested_autologin;
 			$writable = !!($autologin && $autologin[0]);
 			if ($realname == '')
@@ -240,7 +239,7 @@ BLOCK;
 			$q_realname = htmlspecialchars($realname);
 			$show_edit = ($this->openid == $owner_userid && !$this->isAdmin() ? '' : 'ui-helper-hidden');
 			$show_admin_edit = ($this->isAdmin() ? '' : 'ui-helper-hidden');
-			$output .= "\t<tr class='" .($this->openid == $owner_userid ? 'mine ' : '') . (!$readable ? 'nonreadable ' : '') . (!$writable ? 'nonwritable' : '') . (!$active ? 'inactive' : '') . "'>".
+			$output .= "\t<tr class='" .($this->openid == $owner_userid ? 'mine ' : '') . (!$readable ? 'nonreadable ' : '') . (!$writable ? 'nonwritable' : '') . (!$isactive ? 'inactive' : '') . "'>".
 				"<td class='minwidth nowrap' style='text-align:right'>$wikiid</td>".
 				"<td class='minwidth nowrap'>".($readable ? "<a href=\"/$wikiname/\">$wikiname</a>" : $wikiname)."</td>".
 				"<td class='minwidth nowrap'>".($writable ? "<span class='ui-icon ui-icon-pencil' style='float:right; vertical-align:bottom;'></span>" : "" )."</td>".
@@ -862,6 +861,13 @@ BLOCK;
 		}
 		$html .= "</tbody></table>";
 
+		$html .= "<div style=\"min-height: 8px;\" />";
+		$html .= $this->textHighlight ("By default, inactive wikis are not shown in list of wikis on the dashboard.", "home");
+        $checked = $isactive ? "checked" : "";
+		$html .= <<<BLOCK
+        <input type="checkbox" class="generic_ajax" ga_form_id="mwf{$wikiid}" ga_action="managewiki_isactive" id="mw{$wikiid}_isactive" name="mw{$wikiid}_isactive" value="1" refresh_tab="#tabs" $checked />This wiki is active
+BLOCK;
+
 		$html .= "<br /><div style=\"min-height: 12px;\" /><br />";
 
 		$html .= "</form>";
@@ -1280,6 +1286,23 @@ EOT;
                       "refreshtab" => $refreshrule,
                       "refreshdiv" => $refreshdiv);
     }
+
+	function ajax_managewiki_isactive ($post) {
+		$wikiid = $post["wikiid"];
+		$wikiid0 = sprintf ("%02d", $wikiid);
+		$wiki = $this->getWiki($wikiid);
+		if (!is_array ($wiki))
+			return $this->fail ("No wiki specified.");
+		if (!$this->isAdmin() && $wiki["userid"] != $this->openid)
+			return $this->fail ("You are not allowed to do that.");
+
+        $isactive = @$post[$post["ga_button_id"]];
+        $this->setActiveFlag($wikiid, $isactive);
+
+		return array ("success" => true,
+                      "check" => $isactive ? [$post["ga_button_id"]] : [],
+                      "uncheck" => !$isactive ? [$post["ga_button_id"]] : []);
+	}
 
 	function ajax_managewiki_groups ($post) {
 		$wikiid = $post["wikiid"];

WikifarmDriver.php
@@ -234,6 +234,7 @@ class WikifarmDriver {
 			wikis.realname as realname,
 			wikis.userid as userid,
 			wikis.userid as owner_userid,
+            wikis.isactive as isactive,
 			users.realname as owner_realname
 			FROM wikis 
 			LEFT JOIN users ON users.userid = wikis.userid
@@ -399,6 +400,12 @@ class WikifarmDriver {
 			return false;
 		return true;
 	}
+
+    function setActiveFlag ($wikiid, $isactive=true) {
+        $wikiid += 0;
+        $isactive = $isactive ? 1 : 0;
+		$this->DB->exec ("update wikis set isactive='$isactive' where id=$wikiid");
+    }
 
 	// Affects a group's relationship with a wiki
 	function inviteGroup ($wikiid, $groupid, $readonly=true) {

initialize_wikis_db.php
@@ -27,7 +27,7 @@ $db->exec ('CREATE TABLE sitepref (
  prefid varchar(64),
  value varchar(255),
  description varchar(255))');
-if ($db->exec ('CREATE UNIQUE INDEX sp ON sitepref (prefid)')) {
+if (@$db->exec ('CREATE UNIQUE INDEX sp ON sitepref (prefid)')) {
     $db->exec ('INSERT INTO sitepref (prefid, value) VALUES ("allow_mw_native_login", "0")');
     $db->exec ('INSERT INTO sitepref (prefid, value) VALUES ("unreadable_wikis_visible", "1")');
 }
@@ -36,6 +36,10 @@ $db->exec ('INSERT OR IGNORE INTO sitepref (prefid, value) VALUES ("default_wiki
 @$db->exec ('ALTER TABLE usergroups ADD isadmin INTEGER DEFAULT 0');
 if(@$db->exec ('ALTER TABLE pref ADD defaultvalue varchar(255)')) {
     $db->exec ('UPDATE pref SET defaultvalue=1 WHERE prefid="admin_notify_requests"');
+}
+
+if(@$db->exec ('ALTER TABLE wikis ADD isactive INTEGER DEFAULT 1')) {
+    $db->exec ('UPDATE wikis SET isactive=0 WHERE userid not like "%://%"');
 }
 
 if (!@$db->exec ('CREATE TABLE userpref (
@@ -74,8 +78,9 @@ if (!$db->exec ('CREATE TABLE wikis (
  wikiname varchar(32),
  userid varchar(255),
  realname varchar(128),
- unique (wikiname)
- )'))
+ unique (wikiname),
+ isactive INTEGER DEFAULT 1
+)'))
     die ($db->lastErrorMsg());
 
 if (!$db->exec ('CREATE TABLE users (

commit b65ef086175c35369d32a10d2627c6a54f0a65df

fix array syntax (bug introduced in 2ffd4c11e1d8fc484f2fbe6a31eb9f767ea424d4)

WikifarmPageMachine.php
@@ -1300,8 +1300,8 @@ EOT;
         $this->setActiveFlag($wikiid, $isactive);
 
 		return array ("success" => true,
-                      "check" => $isactive ? [$post["ga_button_id"]] : [],
-                      "uncheck" => !$isactive ? [$post["ga_button_id"]] : []);
+                      "check" => $isactive ? array($post["ga_button_id"]) : array(),
+                      "uncheck" => !$isactive ? array($post["ga_button_id"]) : array());
 	}
 
 	function ajax_managewiki_groups ($post) {